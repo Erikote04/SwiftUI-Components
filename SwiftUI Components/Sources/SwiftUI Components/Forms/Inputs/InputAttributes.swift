ea268626ed7db2ca42b150fd295e5295b7669be5
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-03-11 21:17:53 +0100

    Deleted duplicated files

--- SwiftUI Components/Sources/SwiftUI Components/Forms/Inputs/InputAttributes.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-enum InputAttributes {
-    enum InputStyle { case checkbox, radio, radioCheck }
-    enum ImageSize: CGFloat {
-        /// size: 24
-        case `default` = 24
-    }
-}
