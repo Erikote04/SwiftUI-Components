c85ab70ec92183874d04e17f22462a2de43252a6
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-03-11 20:54:19 +0100

    Added documentation for helpers

--- Sources/SwiftUIComponents/Helpers/View/View+Condition.swift
+++ Sources/SwiftUIComponents/Helpers/View/View+Condition.swift
@@ -8,10 +8,22 @@ public extension View {
     ///   - condition: The condition to evaluate.
     ///   - transform: The transform to apply to the source `View`.
     /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-    @ViewBuilder
-    func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
         if condition {
             transform(self)
+        } else {
+            self
+        }
+    }
+    
+    /// Applies the given transform if the given optional has a value.
+    /// - Parameters:
+    ///   - value: The optional value to unwrap and evaluate.
+    ///   - transform: The transform to apply to the unwrapped value.
+    /// - Returns: Either the original `View` or the modified `View` if the optional has a value.
+    @ViewBuilder func ifLet<Value, Content: View>(_ value: Value?, transform: (Self, Value) -> Content) -> some View {
+        if let value {
+            transform(self, value)
         } else {
             self
         }
